85dbc2987e37b15835f7eba686a22628c0d05667
Ian Hough <ijhough@gmail.com>
2010-07-28 14:59:33 -0700

    Associations

--- lib/test_support/purchase_record.rb
+++ lib/test_support/purchase_record.rb
@@ -6,4 +6,10 @@ require 'sniff'
 class PurchaseRecord < ActiveRecord::Base
   include Sniff::Emitter
   include BrighterPlanet::Purchase
+  
+  belongs_to :merchant,          :foreign_key => 'merchant_id'
+  belongs_to :merchant_category, :foreign_key => 'mcc'
+  # belongs_to :industry,          :foreign_key => 'naics_code'
+  # belongs_to :product_line,      :foreign_key => 'pscode'
+  # belongs_to :sector,            :foreign_key => 'io_code'
 end
