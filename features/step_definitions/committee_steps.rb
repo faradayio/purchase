4d2eee47215e5d10a1b9aa4bbc1069f0363b2760
Derek Kastner <dkastner@gmail.com>
2010-07-29 17:08:46 -0400

    Setting up a test and implementing industry_shares committee

--- features/step_definitions/committee_steps.rb
+++ features/step_definitions/committee_steps.rb
@@ -1,18 +1,33 @@
+def coerce_value(value)
+  if value =~ /\d+\.\d+/
+    value.to_f
+  elsif value =~ /^\d+$/
+    value.to_i
+  else
+    value
+  end
+end
+
 Given /^a purchase emitter$/ do
   @activity = PurchaseRecord
 end
 
 Given /^(a )?characteristic "(.*)" of "(.*)"$/ do |_, name, value|
   @characteristics ||= {}
-  if value =~ /\d+\.\d+/
-    value = value.to_f
-  elsif value =~ /^\d+$/
-    value = value.to_i
+
+  if name =~ /\./
+    model_name, attribute = name.split /\./
+    model = model_name.singularize.camelize.constantize
+    value = model.send "find_by_#{attribute}", value
+    @characteristics[model_name.to_sym] = value
+  else
+    value = coerce_value(value)
+    @characteristics[name.to_sym] = value
   end
-  @characteristics[name.to_sym] = value
 end
 
 When /^the (.*) committee is calculated$/ do |committee_name|
+  puts "calcing #{committee_name} committee with chars #{@characteristics.inspect}"
   @decision ||= @activity.decisions[:emission]
   @committee = @decision.committees.find { |c| c.name.to_s == committee_name }
   @report = @committee.report(@characteristics, [])
@@ -32,4 +47,10 @@ Then /^the conclusion of the committee should be (.+)$/ do |conclusion|
   else
     @report.conclusion.should == conclusion
   end
+end
+
+Then /^the conclusion of the committee should include a key of (.*) and value (.*)$/ do |key, value|
+  @report.conclusion.keys.should include(key)
+
+  @report.conclusion[key].should == value
 end
