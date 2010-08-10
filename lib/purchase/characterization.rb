2cef1bf2bfc4e5f227d6940d7ec58bc4598321e5
Derek Kastner <dkastner@gmail.com>
2010-08-10 14:05:57 -0400

    Our finalized set of characteristics

--- lib/purchase/characterization.rb
+++ lib/purchase/characterization.rb
@@ -8,15 +8,10 @@ module BrighterPlanet
         base.characterize do
           has :merchant
           has :merchant_category
-          has :industry_shares 
-          has :product_line_shares
-          has :sector_shares
-          has :purchase_amount # full purchase amount
+          has :industry
+          has :total           # full purchase amount
           has :tax             # tax portion of purchase
           has :cost            # cost before tax
-          has :line_item       # text describing what was purchased
-          has :customer_code
-          has :zip_code
           has :date
         end
       end
