bc3fccbd64a4a712ba2d490e341664e6a98d03ba
Derek Kastner <dkastner@gmail.com>
2010-08-24 17:34:16 -0400

    Mostly passing impacts committee (finish tests)

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,5 +6,3 @@ require 'cucumber/formatter/unicode' # Remove this line if you don't want Cucumb
 
 require 'sniff'
 Sniff.init File.join(File.dirname(__FILE__), '..', '..'), :earth => :industry, :cucumber => true
-
-require 'purchase/test_sector_direct_requirements_adapter'
--- lib/purchase.rb
+++ lib/purchase.rb
@@ -8,10 +8,23 @@ module BrighterPlanet
 
     class << self
       def sector_direct_requirements_adapter
-        @sector_direct_requirements_adapter ||= TestSectorDirectRequirementsAdapter
+        return @sector_direct_requirements_adapter if @sector_direct_requirements_adapter
+
+        require 'test_support/test_sector_direct_requirements_adapter'
+        @sector_direct_requirements_adapter = TestSectorDirectRequirementsAdapter
       end
       def sector_direct_requirements_adapter=(val)
         @sector_direct_requirements_adapter = val
+      end
+
+      def impact_vectors_adapter
+        return @impact_vectors_adapter if @impact_vectors_adapter
+
+        require 'test_support/test_impact_vectors_adapter'
+        @impact_vectors_adapter = TestImpactVectorsAdapter
+      end
+      def impact_vectors_adapter=(val)
+        @impact_vectors_adapter = val
       end
     end
   end
