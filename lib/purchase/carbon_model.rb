815bf5f46c753881275b1fe13a7195e990b7c922
Derek Kastner <dkastner@gmail.com>
2010-08-25 10:43:18 -0400

    Moved Matrix/Vector extensions into separate file, moved matrix steps into step definitions

--- lib/purchase/carbon_model.rb
+++ lib/purchase/carbon_model.rb
@@ -251,24 +251,3 @@ module BrighterPlanet
     KEY_MAP = (1..26).to_a.map(&:to_s) + %w{44100 44101 44102 44103 44104 44105}
   end
 end
-
-class Matrix
-  def inspect
-    str = "Matrix:\n"
-    keys = BrighterPlanet::Purchase::KEY_MAP
-    str += '|' + (['X'.center(5)] + keys.map { |k| k.center(5) }).join('|') + "|\n"
-    @rows.each_with_index do |row, i|
-      str += '|' + ([keys[i].center(5)] + row.map { |r| r.to_s.center(5) }).join('|') + "|\n"
-    end
-    str
-  end
-end
-class Vector
-  def inspect
-    str = "Vector:\n"
-    keys = BrighterPlanet::Purchase::KEY_MAP
-    str += '|' + (keys.map { |k| k.center(5) }).join('|') + "|\n"
-    str += '|' + (to_a.map { |r| r.to_s.center(5) }).join('|') + "|\n"
-    str
-  end
-end
