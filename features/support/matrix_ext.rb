cd886fb71a80a9fe149196b9a04c55c91c2f888c
Derek Kastner <dkastner@gmail.com>
2010-08-25 11:53:02 -0400

    Gussied up the output of Matrix#inspect and Vector#inspect

--- features/support/matrix_ext.rb
+++ features/support/matrix_ext.rb
@@ -1,20 +1,38 @@
+class MatrixVectorHelper
+  def self.column_width(rows)
+    rows.map(&:to_s).map(&:length).max + 2
+  end
+end
+
 class Matrix
   def inspect
-    str = "Matrix:\n"
     keys = BrighterPlanet::Purchase::KEY_MAP
-    str += '|' + (['X'.center(5)] + keys.map { |k| k.center(5) }).join('|') + "|\n"
+    paddings = {}
+    keys.each_with_index do |key, i|
+      paddings[key] = MatrixVectorHelper.column_width(column(i).to_a + [key])
+    end
+    str = "Matrix:\n"
+    str += '|' + (['X'.center(5)] + keys.map { |k| k.center(paddings[k]) }).join('|') + "|\n"
     @rows.each_with_index do |row, i|
-      str += '|' + ([keys[i].center(5)] + row.map { |r| r.to_s.center(5) }).join('|') + "|\n"
+      items = [keys[i].center(5)]
+      row.each_with_index { |i, col| items << i.to_s.center(paddings[keys[col]]) }
+      str += '|' + items.join('|') + "|\n"
     end
     str
   end
 end
 class Vector
   def inspect
-    str = "Vector:\n"
     keys = BrighterPlanet::Purchase::KEY_MAP
-    str += '|' + (keys.map { |k| k.center(5) }).join('|') + "|\n"
-    str += '|' + (to_a.map { |r| r.to_s.center(5) }).join('|') + "|\n"
+    paddings = {}
+    keys.each_with_index do |key, i|
+      paddings[key] = MatrixVectorHelper.column_width([to_a[i],key])
+    end
+    str = "Vector:\n"
+    str += '|' + (keys.map { |k| k.center(paddings[k]) }).join('|') + "|\n"
+    items = []
+    to_a.each_with_index { |r, i| items << r.to_s.center(paddings[keys[i]]) }
+    str += '|' + items.join('|') + "|\n"
     str
   end
 end
