b21222e25b6faca30f29d482fe6497c83dfb9f11
Seamus Abshere <seamus@abshere.net>
2011-06-28 18:51:59 -0500

    Revert "Fixed characterization using #displays"
    This reverts commit bcf1216c52d5e6a2182ef0cf1d329ee73ef1492f.

--- lib/purchase/characterization.rb
+++ lib/purchase/characterization.rb
@@ -12,55 +12,55 @@ module BrighterPlanet
           has :tax             # tax portion of purchase
           has :date
           has :merchant do |m|
-            m.displays { |m| m.name }
+             m.name
           end
           has :merchant_category do |mc|
-            mc.displays { |mc| mc.description }
+             mc.description
           end
           has :merchant_category_industries do |mci|
-            mci.displays { |mc| pluralize(mci.count, 'merchant category') }
+             pluralize(mci.count, 'merchant category')
           end
           has :industry do |ind|
-            ind.displays { |mc| ind.description }
+             ind.description
           end
           has :trade_industry_ratios do |tir|
-             tir.displays { || pluralize(tir.count, 'trade industry ratio') }
+             pluralize(tir.count, 'trade industry ratio')
           end
           has :non_trade_industry_ratios do |ntir|
-             ntir.displays { || pluralize(ntir.count, 'non-trade industry ratio') }
+             pluralize(ntir.count, 'non-trade industry ratio')
           end
           has :product_line_ratios do |plr|
-             plr.displays { || pluralize(plr.count, 'product line ratio') }
+             pluralize(plr.count, 'product line ratio')
           end
           has :product_line_industry_product_ratios do |plipr|
-             plipr.displays { |plipr| pluralize(plipr.count, 'product line industry product ratio') }
+             pluralize(plipr.count, 'product line industry product ratio')
           end
           has :industry_product_ratios do |ipr|
-             ipr.displays { |ipr| pluralize(ipr.count, 'industry product ratio') }
+             pluralize(ipr.count, 'industry product ratio')
           end
           has :industry_ratios do |ir|
-             ir.displays { |ir| pluralize(ir.count, 'industry ratio') }
+             pluralize(ir.count, 'industry ratio')
           end
           has :industry_sector_ratios do |isr|
-             isr.displays { |isr| pluralize(isr.count, 'industry sector ratio') }
+             pluralize(isr.count, 'industry sector ratio')
           end
           has :industry_sector_shares do |isr|
-             isr.displays { |isr| pluralize(isr.count, 'industry sector share') }
+             pluralize(isr.count, 'industry sector share')
           end
           has :sector_shares do |isr|
-            isr.displays { |isr| "#{isr.size}-element sector shares vector" }
+            "#{isr.size}-element sector shares vector"
           end
           has :sector_direct_requirements do |sdr|
-            sdr.displays { |sdr| "#{sdr.row_size}x#{sdr.column_size} sector direct requirements matrix" }
+            "#{sdr.row_size}x#{sdr.column_size} sector direct requirements matrix"
           end
           has :economic_flows do |ef|
-            ef.displays { |ef| "#{ef.size}-element economic flows vector" }
+            "#{ef.size}-element economic flows vector"
           end
           has :impact_vectors do |iv|
-            iv.displays { |iv| "#{iv.row_size}x#{iv.column_size} impact vector" }
+            "#{iv.row_size}x#{iv.column_size} impact vector"
           end
           has :impacts do |i|
-            i.displays { |i| "#{i.size}-element impacts vector" }
+            "#{i.size}-element impacts vector"
           end
         end
       end
