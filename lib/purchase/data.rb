0ad7ddf6fad4a6bf7e9f869e5a1c323559aa57a7
Seamus Abshere <seamus@abshere.net>
2012-05-02 23:53:24 -0500

    remove unused units columns

--- lib/purchase/data.rb
+++ lib/purchase/data.rb
@@ -3,11 +3,8 @@ module BrighterPlanet
     module Data
       def self.included(base)
         base.col :cost, :type => :float
-        base.col :cost_units
         base.col :purchase_amount, :type => :float
-        base.col :purchase_amount_units
         base.col :tax, :type => :float
-        base.col :tax_units
         base.col :date, :type => :date
         base.col :merchant_id
         base.col :mcc
