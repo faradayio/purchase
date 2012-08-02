da617118af6a9e1c5c022506e604bcac335f5d96
Derek Kastner <dkastner@gmail.com>
2012-08-02 16:26:30 -0500

    upgrade to latest Sniff

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,21 +1,6 @@
-require 'bundler'
-Bundler.setup
-
-require 'cucumber'
+require 'bundler/setup'
 
 require 'sniff'
-Sniff.init File.join(File.dirname(__FILE__), '..', '..'),
-  # :adapter => 'mysql2',
-  # :database => 'test_flight',
-  # :username => 'root',
-  # :password => 'password',
-  :earth => :industry,
+Sniff.init File.expand_path('../../..', __FILE__),
   :cucumber => true,
-  :logger => 'log/test_log.txt'
-
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'test_impact_vectors_adapter'
-require 'test_sector_direct_requirements_adapter'
-
-BrighterPlanet::Purchase.sector_direct_requirements_adapter = BrighterPlanet::Purchase::TestSectorDirectRequirementsAdapter
-BrighterPlanet::Purchase.impact_vectors_adapter = BrighterPlanet::Purchase::TestImpactVectorsAdapter
+  :logger => false # change this to $stderr to see database activity
